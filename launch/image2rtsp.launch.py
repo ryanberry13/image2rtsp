2e2b76e8331328158568b60360a184358f336f8f
ryanberry13 <ryanberry1303@gmail.com>
2025-05-13 09:00:05 -0400

    multilaunch changes and pipeline config

--- launch/image2rtsp.launch.py
+++ launch/image2rtsp.launch.py
@@ -4,17 +4,33 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-   config = os.path.join(
+   rgb_config = os.path.join(
       get_package_share_directory('image2rtsp'),
       'config',
-      'parameters.yaml'
+      'rgb_parameters.yaml'
+      )
+   
+   ir_config = os.path.join(
+      get_package_share_directory('image2rtsp'),
+      'config',
+      'ir_parameters.yaml'
       )
 
    return LaunchDescription([
       Node(
          package='image2rtsp',
          executable='image2rtsp',
+         namespace='rgb_camera',
          name='image2rtsp',
-         parameters=[config]
+         parameters=[rgb_config],
+         output='screen',
+      ),
+      Node(
+         package='image2rtsp',
+         executable='image2rtsp',
+         namespace='ir_camera',
+         name='image2rtsp',
+         parameters=[ir_config],
+         output='screen',
       )
    ])
--- src/image2rtsp.cpp
+++ src/image2rtsp.cpp
@@ -39,14 +39,14 @@ Image2rtsp::Image2rtsp() : Node("image2rtsp"){
     rtsp_server = rtsp_server_create(port, local_only);
     appsrc = NULL;
     // Setup the pipeline
-    pipeline_tail = " key-int-max=30 ! video/x-h264, profile=baseline ! rtph264pay name=pay0 pt=96 )";
+    pipeline_tail = " gop-size=30 ! video/x-h264, profile=baseline ! rtph264pay name=pay0 pt=96 )";
     if (camera == false){
         pipeline_head = "( appsrc name=imagesrc do-timestamp=true min-latency=0 max-latency=0 max-bytes=1000 is-live=true ! videoconvert ! videoscale ! ";
         pipeline = pipeline_head + caps_1 + framerate + caps_2 + " ! x264enc tune=zerolatency bitrate=" + bitrate + pipeline_tail;
         rtsp_server_add_url(mountpoint.c_str(), pipeline.c_str(), (GstElement **)&(appsrc));
     }
     else {
-        pipeline = "( " + source + " ! videoconvert ! videoscale ! " + caps_1 + framerate + caps_2 + " ! x264enc tune=zerolatency bitrate=" + bitrate + pipeline_tail;
+        pipeline = "( " + source + " ! videoconvert ! videoscale ! " + caps_1 + framerate + caps_2 + " ! nvh264enc preset=low-latency bitrate=" + bitrate + pipeline_tail;
         rtsp_server_add_url(mountpoint.c_str(), pipeline.c_str(), NULL);
     }
     RCLCPP_INFO(this->get_logger(), "Stream available at rtsp://%s:%s%s", gst_rtsp_server_get_address(rtsp_server), port.c_str(), mountpoint.c_str());
